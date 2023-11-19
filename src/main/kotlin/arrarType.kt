f3e3339d1f3a59301d8a469a0d15c6896e8ea775
tobisamcode <samueloluwatobiloba48@gmail.com>
2023-11-19 17:24:17 +0100

    array data types

---
+++ src/main/kotlin/arrarType.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    // to create an array of integers 1,2 and 3
+    val myArray: Array<Int> = arrayOf(1,2,3)
+    println(myArray[0])
+    println("get the size of myArray: ${myArray.size}")
+}
