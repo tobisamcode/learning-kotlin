2bf54fc50b5312561db34c8c4dcedde945cf1980
tobisamcode <samueloluwatobiloba48@gmail.com>
2023-11-22 01:22:56 +0100

    functions

---
+++ src/main/kotlin/3 - functions/Game.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val options = arrayOf("Rock", "Paper", "Scissors")
+
+    val gameChoice = getGameChoice(options)
+}
+
+fun getGameChoice(optionsParam: Array<String>) =
+    optionsParam[(Math.random() * optionsParam.size).toInt()]
