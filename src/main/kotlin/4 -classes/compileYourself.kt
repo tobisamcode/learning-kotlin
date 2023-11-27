8ac2971a202d714cfb086f75435addf0b27cb7f2
tobisamcode <samueloluwatobiloba48@gmail.com>
2023-11-27 08:02:39 +0100

    compile yourself exercise 4

--- src/main/kotlin/4 -classes/compileYourself.kt
+++ src/main/kotlin/4 -classes/compileYourself.kt
@@ -1,2 +1,21 @@
 package `4 -classes`
+class TapeDeck{
+    var hasRecorder: Boolean = false
 
+    fun playTape() {
+        println("Tape playing")
+    }
+
+    fun  recordTape() {
+        if (hasRecorder) {
+            println("Tape recording!")
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    var t = TapeDeck()
+    t.hasRecorder = true
+    t.playTape()
+    t.recordTape()
+}
--- src/main/kotlin/4 -classes/compileYourself2.kt
+++ src/main/kotlin/4 -classes/compileYourself2.kt
@@ -1,4 +1,21 @@
 package `4 -classes`
 
-class compileYourself2 {
+class DVDPlayer(var hasRecorder: Boolean) {
+    fun recordDVD () {
+        if (hasRecorder) {
+            println("DVD recording")
+        }
+    }
+
+    fun playDVD () {
+        if (hasRecorder) {
+            println("DVD playing!")
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val d = DVDPlayer(true)
+    d.recordDVD()
+    d.playDVD()
 }
