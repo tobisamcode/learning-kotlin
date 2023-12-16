86ea0403fe9117a005e176d26a0903ff7709f53b
tobisamcode <samueloluwatobiloba48@gmail.com>
2023-12-16 23:01:17 +0100

    abstract classes

---
+++ src/main/kotlin/6 - abstract classes/Animals.kt
@@ -0,0 +1,73 @@
+abstract class Animals {
+    abstract val image: String
+    abstract val food: String
+    abstract val habitat: String
+    var hunger = 10
+
+    abstract fun makeNoise()
+    abstract fun eat()
+
+    open fun roam() {
+        println("The Animal is roaming")
+    }
+
+    open fun sleeping() {
+        println("The Animal is sleeping")
+    }
+}
+
+class Hippos : Animals() {
+    override val image = "hippo.png"
+    override val food = "grass"
+    override val habitat = "water"
+
+    override fun makeNoise() {
+        println("Grunt! Grunt!")
+    }
+
+    override fun eat() {
+        println("The hippo is eating $food")
+    }
+}
+
+abstract class Canines : Animals() {
+    override fun roam() {
+        println("The canine is roaming!")
+    }
+}
+
+class Wolfs : Canines() {
+    override val image = "wolf.png"
+    override val food = "forests"
+    override val habitat = "forests"
+
+    override fun makeNoise() {
+        println("Hooooools!")
+    }
+
+    override fun eat() {
+        println("The wolf is eating $food")
+    }
+}
+
+class Vets {
+    fun giveShot(animal: Animals) {
+        animal.makeNoise()
+    }
+}
+
+fun main(args: Array<String>) {
+    val animals = arrayOf(Hippos(), Wolfs())
+
+    for (item in animals) {
+        item.roam()
+        item.eat()
+    }
+
+    val vet = Vets()
+    val wolf = Wolfs()
+    val hippo = Hippos()
+
+    vet.giveShot(wolf)
+    vet.giveShot(hippo)
+}
